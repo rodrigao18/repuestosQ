cb17a666d9b1f69b8853e43af8f774b07f008490
rodrigao18 <rfigueroa.rodrigo@gmail.com>
2020-02-02 18:59:17 -0300

    up

--- ver_ventas_tarjetas.php
+++ ver_ventas_tarjetas.php
@@ -56,7 +56,7 @@
 		
 				<div class="form-group col-md-6">
 				<label>Fecha Término</label>
-					<input type="date" class="form-control" id="fecha_inicio" onchange=cargar_ventas_onchange() min="2013-01-01" max="2025-12-31" value="<?php echo date("Y-m-d",strtotime($fecha_actual."- 10 days"));?>">
+					<input type="date" class="form-control" id="fecha_inicio" onchange=cargar_ventas_onchange() min="2013-01-01" max="2025-12-31" value="<?php echo date("Y-m-d");?>">
 				</div>
 			
 				<div class="form-group col-md-6">
